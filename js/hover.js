9e231fc9f47c1d50866e9d0ca9bfef7137925e2a
reas92 <romil080@gmail.com>
2022-03-10 17:31:23 +0100

    Edited JS and edited file placemant of JS

---
+++ js/hover.js
@@ -0,0 +1,7 @@
+var myAnimation = new hoverEffect({
+  parent: document.querySelector('.img'),
+  intensity: .3,
+  image1: '../assets/abstract1.jpg',
+  image2: '../assets/abstract2.jpg',
+  displacementImage: '../assets/dis.jpg'
+});
